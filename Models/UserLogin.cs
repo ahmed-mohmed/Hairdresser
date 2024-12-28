540eafb091ca9ed2685f3a9d4f8e6afc24f54f49
ahmed-mohmed <ahmedd01544@gmail.com>
2024-12-28 23:22:05 +0300

    update

--- Models/UserLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hairdresser.Models
-{
-    public class UserLogin
-    {
-            public string Username { get; set; }
-            public string Password { get; set; }
-      
-    }
-}
